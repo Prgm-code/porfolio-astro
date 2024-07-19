e4307963180cc45c33345a9f523798c89b2326d7
Patricio <patricio@prgmdev.com>
2024-07-19 04:03:23 -0400

    Refactor variable names, update comments, and improve code organization

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,17 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class', // or 'media' or 'class
 	theme: {
-		extend: {},
-	},
-	plugins: [],
+		extend: {
+			animation: {
+				'fade-in': 'fadeIn 2s ease-in-out',
+			},
+			keyframes: {
+				fadeIn: {
+					'0%': { opacity: 0 },
+					'100%': { opacity: 1 },
+				},
+			},
+		},
+		plugins: [],
+	}
 }
