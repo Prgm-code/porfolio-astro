6e30213120ae80ee48f7ddcbb5d31539fe5205b9
PRGM <pgomez.meneses@gmail.com>
2025-09-29 02:05:18 -0300

    feat: add Vercel analytics and deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from "astro/config";
+// import vercel from "@astrojs/vercel/serverless";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  // output: "server",
+  // adapter: vercel({
+  //   webAnalytics: {
+  //     enabled: true, // set to false when using @vercel/analytics@1.4.0
+  //   },
+  // }),
   integrations: [],
 
   vite: {
